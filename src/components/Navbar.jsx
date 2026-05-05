a9b7e7f3782c2bc0a5873b41ba94c7ed5049cbd1
Mohamed Essam <mohamedessam9523@gmail.com>
2026-05-05 21:18:12 +0300

    fix logo bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import logo from "../../public/assets/me.png";
 const Navbar = () => {
   const scrollToSection = (e, id) => {
     e.preventDefault();
@@ -27,7 +27,7 @@ const Navbar = () => {
         className="pointer-events-auto transition-transform duration-300 hover:scale-110 active:scale-95 group"
       >
         <img
-          src="../../public/assets/me.png"
+          src={logo}
           alt="Logo"
           className="w-10 h-10 md:w-12 md:h-12 rounded-full object-cover border-2 border-dark-primary/20 shadow-[0_0_15px_rgba(255,255,255,0.1)] group-hover:shadow-dark-primary/30 transition-all"
         />
